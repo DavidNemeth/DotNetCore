8d9eb8bc43936922373eabcc94ad73c1ea25e2a8
David Nemeth <dt-dave@hotmail.com>
2017-09-28 13:09:39 +0200

    Angular data binding.

---
+++ Blank/Blank/wwwroot/js/app-trips.js
@@ -0,0 +1,8 @@
+﻿(function () {
+
+    "use strict";
+
+    //creating the module
+    angular.module("app-trips",[]);
+
+})();
---
+++ Blank/Blank/wwwroot/js/tripsController.js
@@ -0,0 +1,33 @@
+﻿(function () {
+
+    "user strict";
+
+    //Getting the existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+
+    function tripsController() {
+
+        var vm = this;
+
+        vm.trips = [{
+            name: "Us Trip",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            created: new Date()
+        }, {
+            name: "Europe Trip",
+            created: new Date()
+        }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            alert(vm.newTrip.name);
+        };
+
+    }
+
+})();
