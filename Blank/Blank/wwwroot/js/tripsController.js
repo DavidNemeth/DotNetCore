591590659b3d3358629760910b3c7130c898a4ae
David Nemeth <dt-dave@hotmail.com>
2017-09-28 13:45:08 +0200

    Connecting Angular to Api endpoint

--- Blank/Blank/wwwroot/js/tripsController.js
+++ Blank/Blank/wwwroot/js/tripsController.js
@@ -7,22 +7,29 @@
         .controller("tripsController", tripsController);
 
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips = [{
-            name: "Us Trip",
-            created: new Date()
-        }, {
-            name: "World Trip",
-            created: new Date()
-        }, {
-            name: "Europe Trip",
-            created: new Date()
-        }];
+        vm.trips = [];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                //success
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                //failure
+                vm.errorMessage = "Failed to load data: " + error;                
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
 
         vm.addTrip = function () {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
