8125088b826f65d4373dd52422870cc23daf48ad
Dávid Németh <david.nemeth90@gmail.com>
2018-01-22 15:56:09 +0100

    Added sample Angular material component

--- Skeleta/SkeletaWeb/ClientApp/app/components/home/home.component.ts
+++ Skeleta/SkeletaWeb/ClientApp/app/components/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { startWith, map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+
+export class Country {
+	constructor(public name: string, public population: string, public flag: string) { }
+}
 
 @Component({
 	selector: 'home',
@@ -30,7 +36,19 @@ export class HomeComponent {
 		this.isLinear = !this.isLinear;
 	}
 
-	constructor(private _formBuilder: FormBuilder) { }
+	constructor(private _formBuilder: FormBuilder) {
+		this.countryCtrl = new FormControl();
+		this.filteredCountries = this.countryCtrl.valueChanges
+			.pipe(
+			startWith(''),
+			map(country => country ? this.filtercountrys(country) : this.countrys.slice())
+			);
+	}
+
+	filtercountrys(name: string) {
+		return this.countrys.filter(country =>
+			country.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+	}
 
 	ngOnInit() {
 		this.firstFormGroup = this._formBuilder.group({
@@ -49,5 +67,31 @@ export class HomeComponent {
 		{ name: 'Cat', sound: 'Meow!' },
 		{ name: 'Cow', sound: 'Moo!' },
 		{ name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!' },
+	];
+
+	countryCtrl: FormControl;
+	filteredCountries: Observable<any[]>;
+
+	countrys: Country[] = [
+		{
+			name: 'Hungary',
+			population: '9.818M',			
+			flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Flag_of_Hungary.svg/125px-Flag_of_Hungary.svg.png'
+		},
+		{
+			name: 'England',
+			population: '53.01M',
+			flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/125px-Flag_of_England.svg.png'
+		},
+		{
+			name: 'USA',
+			population: '323.1M',
+			flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/125px-Flag_of_the_United_States.svg.png'
+		},
+		{
+			name: 'China',
+			population: '1412.6M',
+			flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/125px-Flag_of_the_People%27s_Republic_of_China.svg.png'
+		}
 	];
 }
